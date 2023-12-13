fe46f3cf35d125972e8cbf157b9dd2f9f0339d52
Jcasanova1990 <jeremykcasanova@gmail.com>
2023-12-12 22:03:32 -0500

    'fixed'

--- js/mainscript.js
+++ js/mainscript.js
@@ -18,25 +18,37 @@ function createCounter() {
 
 const counter = createCounter();
 
-function updateCounter() {
-    const newCount = counter("increment");
+function updateCounter(action) {
+    const currentInputValue = parseInt(counterInput.value, 10) || 0;
+
+    let newCount;
+    if (action === "increment") {
+        newCount = currentInputValue + 1;
+    } else if (action === "decrement") {
+        newCount = currentInputValue - 1;
+    }
+
+    // Check if the value is negative and apply the red color
+    if (newCount < 0) {
+        counterValue.style.color = "red";
+    } else {
+        counterValue.style.color = ""; // Reset color to default
+    }
+
     counterValue.textContent = newCount;
     counterInput.value = newCount;
 }
 
-incrementBtn.addEventListener("click", updateCounter);
+incrementBtn.addEventListener("click", () => updateCounter("increment"));
 
-decrementBtn.addEventListener("click", () => {
-    const newCount = counter("decrement");
-    counterValue.textContent = newCount;
-    counterInput.value = newCount;
-});
-
-counterInput.value = counter();
-
+decrementBtn.addEventListener("click", () => updateCounter("decrement"));
 
 counterInput.addEventListener("change", () => {
-    const inputValue = parseInt(counterInput.value, 10) || 0; 
+    const inputValue = parseInt(counterInput.value, 10) || 0;
+    counterValue.style.color = inputValue < 0 ? "red" : ""; // Adjust color based on input value
     counterValue.textContent = inputValue;
     counterInput.value = inputValue;
 });
+
+// Set initial counter value in the input field
+counterInput.value = counter();
